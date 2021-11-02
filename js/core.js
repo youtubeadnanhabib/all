44b20ec12acf3232852eb9eb96c69248b13faba2
youtubeadnanhabib <hellooliviamia@gmail.com>
2021-11-02 20:06:04 +0600

    Task

---
+++ js/core.js
@@ -0,0 +1,31 @@
+jQuery(function($){
+  $(document).ajaxSend(function() {
+    $("#overlay").fadeIn(300);　
+  });
+		
+  $('#button').click(function(){
+    $.ajax({
+      type: 'GET',
+      success: function(data){
+        console.log(data);
+      }
+    }).done(function() {
+      setTimeout(function(){
+        $("#overlay").fadeOut(300);
+      },500);
+    });
+  });	
+
+   $('#button2').click(function(){
+    $.ajax({
+      type: 'GET',
+      success: function(data){
+        console.log(data);
+      }
+    }).done(function() {
+      setTimeout(function(){
+        $("#overlay").fadeOut(300);
+      },500);
+    });
+  }); 
+});
